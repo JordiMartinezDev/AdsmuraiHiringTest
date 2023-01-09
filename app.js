9f988a793c8f180337ef5e01f949e601b269a04a
Jordi Martinez <jordiupc87@gmail.com>
2023-01-09 11:17:18 +0100

    Api folder setup

---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes);
+
+module.exports = app;
